ec94fdeddc30a730acedae4c85fcfc3753eb702c
Stephen Feather <sfeather@gmail.com>
2023-03-10 00:23:19 -0500

    Add plugin entry point

---
+++ fa-toolkit.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Feather Arms Toolkit
+Version: 1.0
+Description: Collection of wordpress management tools used by Feather Arms.
+Author: Stephen Feather
+*/
+
+// Load the plugin files
+require_once( plugin_dir_path( __FILE__ ) . 'includes/loader.php' );
+
+// Your plugin code goes here
