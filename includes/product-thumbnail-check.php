30e043c8540d7e78bc20622bafa060f8f33d4174
Stephen Feather <sfeather@gmail.com>
2023-03-10 12:25:21 -0500

    cleanup product-thumbnail-check

--- includes/product-thumbnail-check.php
+++ includes/product-thumbnail-check.php
@@ -4,37 +4,37 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 } 
 
-if ( defined( 'WP_CLI' ) ) {
-    WP_CLI::add_command( 'fa:media product-thumbnail-check', 'WP_CLI_Product_Thumbnail_Check' );
-}
+if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+    exit;
+} 
 
-/**
- * Find WooCommerce products that don't have a thumbnail and move them to drafts.
- *
- * ## OPTIONS
- *
- * [--vendor=<vendor>]
- * : The vendor name to filter the results by.
- *
- * [--result_count=<result_count>]
- * : The number of records to process (default is 20)
- * 
- * [--order=<ASC,DEC>]
- * : Change the order of records
- * 
- * ## EXAMPLES
- *
- *     wp fa:media product-thumbnail-check --vendor=ACME
- *     wp fa:media product-thumbnail-check --result_count=5
- * 
- * @param array $args
- * @param array $assoc_args
- * @return void
- */
+if (!function_exists('WP_CLI_product_thumbnail_check')) {
+    
+    /**
+     * Find WooCommerce products that don't have a thumbnail and move them to drafts.
+     *
+     * ## OPTIONS
+     *
+     *  [--vendor=<vendor>]
+     * : The vendor name to filter the results by.
+     *
+     * [--result_count=<result_count>]
+     * : The number of records to process (default is 20)
+     * 
+     * [--order=<ASC,DEC>]
+     * : Change the order of records
+     * 
+     * ## EXAMPLES
+     *
+     *     wp fa:media product-thumbnail-check --vendor=ACME
+     *     wp fa:media product-thumbnail-check --result_count=5
+     * 
+     * @param array $args
+     * @param array $assoc_args
+     * @return void
+    */
 
-if (!function_exists('WP_CLI_Product_Thumbnail_Check')) {
-
-    function WP_CLI_Product_Thumbnail_Check($args, $assoc_args)
+    function WP_CLI_product_thumbnail_check($args, $assoc_args)
     {
         $order = isset($assoc_args['order']) ? $assoc_args['order'] : 'ASC';
         $vendor = isset($assoc_args['vendor']) ? $assoc_args['vendor'] : '';
@@ -91,4 +91,6 @@ if (!function_exists('WP_CLI_Product_Thumbnail_Check')) {
 
         WP_CLI::line("{$processed_count} products moved to drafts.");
     }
+    
+    WP_CLI::add_command( 'fa:media product-thumbnail-check', 'WP_CLI_product_thumbnail_check' );
 }
