dc67b9692c8ddc5223f794521e8e2a3bb1a8f9dc
Stephen Feather <sfeather@gmail.com>
2023-03-10 00:18:47 -0500

    Added loader template

---
+++ includes/loader.php
@@ -0,0 +1,6 @@
+<?php
+// Load the first plugin file
+//require_once( plugin_dir_path( __FILE__ ) . 'first-plugin.php' );
+
+// Load the second plugin file
+//require_once( plugin_dir_path( __FILE__ ) . 'second-plugin.php' );
