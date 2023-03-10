736de09990830820d143f12d4bd93f5a14051959
Stephen Feather <sfeather@gmail.com>
2023-03-10 11:10:10 -0500

    add .php extension to our include

--- includes/loader.php
+++ includes/loader.php
@@ -1,6 +1,6 @@
 <?php
 // Load the first plugin file
-require_once( plugin_dir_path( __FILE__ ) . 'product-thumbnail-check' );
+require_once( plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php' );
 
 // Load the second plugin file
 //require_once( plugin_dir_path( __FILE__ ) . 'second-plugin.php' );
