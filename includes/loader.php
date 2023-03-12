63b7597466a02285ac5eca42bd8504e7a325739d
Stephen Feather <sfeather@gmail.com>
2023-03-11 20:37:51 -0500

    TODO: 1 | ERROR | Missing file doc comment

--- includes/loader.php
+++ includes/loader.php
@@ -1,5 +1,6 @@
 <?php
-// Load the first plugin file
-require_once( plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php' );
+/**  Load the first plugin file. */
+require_once plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php';
+require_once plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php';
+require_once plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php';
+require_once plugin_dir_path( __FILE__ ) . 'find-media-for-product.php';
