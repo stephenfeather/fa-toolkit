47ff7012cadd705e08cf9efd3547cb92cee97793
Stephen Feather <sfeather@gmail.com>
2023-03-16 11:44:03 -0400

    phpcs: fa-toolkit.php

--- fa-toolkit.php
+++ fa-toolkit.php
@@ -1,16 +1,23 @@
 <?php
-/*
-Plugin Name: Feather Arms Toolkit
-Version: 1.0
-Description: Collection of wordpress management tools used by Feather Arms.
-Author: Stephen Feather
-Author URI: http://stephenfeather.com
-License: GNU General Public License v3.0
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
-Copyright: 2023 Stephen Feather
-*/
+/**
+ * Plugin Name: Feather Arms Toolkit
+ * Version: 1.0
+ * Description: Collection of WordPress management tools used by Feather Arms.
+ * Author: Stephen Feather
+ * Author URI: http://stephenfeather.com
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Copyright: 2023 Stephen Feather
+ *
+ * @package FA-Toolkit
+ */
 
-// Load the plugin files
-require_once( plugin_dir_path( __FILE__ ) . 'includes/loader.php' );
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+};
 
-// Your plugin code goes here
+// Load the plugin files.
+require_once plugin_dir_path( __FILE__ ) . 'includes/loader.php';
+
+// Your plugin code goes here.
