800068a698d89fee0305aa20016f04d8e140a727
Stephen Feather <sfeather@gmail.com>
2023-03-16 10:27:04 -0400

    Place our cli includes inside a WP_CLI check.

--- includes/loader.php
+++ includes/loader.php
@@ -1,6 +1,15 @@
 <?php
-/**  Load the first plugin file. */
-require_once plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php';
-require_once plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php';
-require_once plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php';
-require_once plugin_dir_path( __FILE__ ) . 'find-media-for-product.php';
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} 
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	// Only load our CLI utilities if in the CLI.
+	require_once plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php';
+	require_once plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php';
+	require_once plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php';
+	require_once plugin_dir_path( __FILE__ ) . 'fetch-import-product-image.php';
+	require_once plugin_dir_path( __FILE__ ) . 'find-media-for-product.php';
+	require_once plugin_dir_path( __FILE__ ) . 'tools.php';
+}
