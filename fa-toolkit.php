7a4e06c711a57ba8104250196c58c33d4b53a8e3
Stephen Feather <sfeather@gmail.com>
2023-03-16 10:58:43 -0400

    Only load the cli commands if in the CLI

--- fa-toolkit.php
+++ fa-toolkit.php
@@ -4,6 +4,10 @@ Plugin Name: Feather Arms Toolkit
 Version: 1.0
 Description: Collection of wordpress management tools used by Feather Arms.
 Author: Stephen Feather
+Author URI: http://stephenfeather.com
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Copyright: 2023 Stephen Feather
 */
 
 // Load the plugin files
