c90449b98ca336412a497f01ec004e0d0400db1d
Stephen Feather <sfeather@gmail.com>
2023-03-16 11:49:43 -0400

    phpcs: loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * Loads our includes.
+ *
+ * @package FA-Toolkit
+ */
 
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-} 
+};
 
+// Only load our CLI utilities if in the CLI.
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	// Only load our CLI utilities if in the CLI.
+
 	require_once plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php';
 	require_once plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php';
 	require_once plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php';
@@ -13,3 +20,5 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	require_once plugin_dir_path( __FILE__ ) . 'find-media-for-product.php';
 	require_once plugin_dir_path( __FILE__ ) . 'tools.php';
 }
+
+// Load our additional includes.
