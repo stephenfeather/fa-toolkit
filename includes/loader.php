e44b1204c0ab196a169107845d879002b61fbc04
Stephen Feather <sfeather@gmail.com>
2023-03-19 15:07:10 -0400

    Add New Relic. Update loader.

--- includes/loader.php
+++ includes/loader.php
@@ -10,15 +10,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 };
 
+// Load Site Functions.
+
+require_once plugin_dir_path( __FILE__ ) . '/siteFunctions/functions-fa-newrelic.php';
+
 // Only load our CLI utilities if in the CLI.
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
-	require_once plugin_dir_path( __FILE__ ) . 'product-thumbnail-check.php';
-	require_once plugin_dir_path( __FILE__ ) . 'attach-media-to-draft-products.php';
-	require_once plugin_dir_path( __FILE__ ) . 'export-draft-product-image-sources.php';
-	require_once plugin_dir_path( __FILE__ ) . 'fetch-import-product-image.php';
-	require_once plugin_dir_path( __FILE__ ) . 'find-media-for-product.php';
-	require_once plugin_dir_path( __FILE__ ) . 'tools.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/product-thumbnail-check.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/attach-media-to-draft-products.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/export-draft-product-image-sources.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/fetch-import-product-image.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/find-media-for-product.php';
+	require_once plugin_dir_path( __FILE__ ) . '/cli/tools.php';
 }
 
 // Load our additional includes.
