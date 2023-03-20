426e35883b561352a599a4ba6091cde95b70c3bc
Stephen Feather <sfeather@gmail.com>
2023-03-20 17:57:48 -0400

    Modified loader paths for structure change

--- includes/loader.php
+++ includes/loader.php
@@ -19,12 +19,12 @@ require_once plugin_dir_path( __FILE__ ) . 'siteFunctions/functions-attachment-s
 // Only load our CLI utilities if in the CLI.
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 
-	require_once plugin_dir_path( __FILE__ ) . '/cli/product-thumbnail-check.php';
-	require_once plugin_dir_path( __FILE__ ) . '/cli/attach-media-to-draft-products.php';
-	require_once plugin_dir_path( __FILE__ ) . '/cli/export-draft-product-image-sources.php';
-	require_once plugin_dir_path( __FILE__ ) . '/cli/fetch-import-product-image.php';
-	require_once plugin_dir_path( __FILE__ ) . '/cli/find-media-for-product.php';
-	require_once plugin_dir_path( __FILE__ ) . '/cli/tools.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/product-thumbnail-check.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/attach-media-to-draft-products.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/export-draft-product-image-sources.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/fetch-import-product-image.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/find-media-for-product.php';
+	require_once plugin_dir_path( __FILE__ ) . 'cli/tools.php';
 }
 
 // Load our additional includes.
