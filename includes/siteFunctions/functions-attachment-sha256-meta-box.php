61048bc47fd836a4609068b2af876922ca684447
Stephen Feather <sfeather@gmail.com>
2023-04-03 14:09:06 -0400

    Lint and document SHA metabox

--- includes/siteFunctions/functions-attachment-sha256-meta-box.php
+++ includes/siteFunctions/functions-attachment-sha256-meta-box.php
@@ -1,26 +1,31 @@
 <?php
+/**
+ * Creates a block on the attachment page for the SHA hash.
+ *
+ * @package FA-Toolkit
+ * @since 1.0.1
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Add a meta box to the attachment edit page
+// Add a meta box to the attachment edit page.
 function add_attachment_sha256_hash_meta_box() {
 	add_meta_box(
-		'attachment_sha256_hash_meta_box', // Unique ID
-		'SHA256 Hash', // Box title
-		'display_attachment_sha256_hash_meta_box', // Callback function
-		'attachment', // Post type
-		'side', // Context
-		'default' // Priority
+		'attachment_sha256_hash_meta_box', // Unique ID.
+		'SHA256 Hash', // Box title.
+		'display_attachment_sha256_hash_meta_box', // Callback function.
+		'attachment', // Post type.
+		'side', // Context.
+		'default' // Priority.
 	);
 }
 add_action( 'add_meta_boxes_attachment', 'add_attachment_sha256_hash_meta_box' );
 
-// Display the contents of the meta box
+// Display the contents of the meta box.
 function display_attachment_sha256_hash_meta_box( $post ) {
-	// Retrieve the value of the sha256_hash field for the current attachment
-	// $sha256_hash = get_field( 'sha256_hash', $post->ID );
+	// Retrieve the value of the sha256_hash field for the current attachment.
 	$sha256_hash = get_post_meta( $post->ID, 'sha256_hash', true );
 
 	?>
