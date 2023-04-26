17e6250a8b9a2623889cecfd6796c4d9f26d2314
Stephen Feather <sfeather@gmail.com>
2023-04-26 12:01:44 -0400

    Add Clarity script to page head

--- includes/siteFunctions/functions-fa-msclarity.php
+++ includes/siteFunctions/functions-fa-msclarity.php
@@ -10,21 +10,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * Add MS Bing Clarity to the head.
- */
-function add_msclarity_to_head() { ?>
-<!-- Clarity tracking code for https://www.featherarms.com/ -->
-	<script id="msclarity">    
-	(function(c,l,a,r,i,t,y){        
-		c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-		t=l.createElement(r);t.async=1;
-		t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-		y=l.getElementsByTagName(r)[0];
-		y.parentNode.insertBefore(t,y);
-	})(window, document, "clarity", "script", "gjacoq2fjh");
-	</script>
-	<?php
-}
-
-add_action( 'wp_head', 'add_msclarity_to_head' );
+$src = '/wp-content/plugins/fa-toolkit/assets/scripts/msclarity.js';
+wp_enqueue_script( 'fa-msclarity', $src, array(), '1.0.3', true );
