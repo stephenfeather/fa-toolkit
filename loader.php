793492fcec4f370afe52deee26dd9cf7568e4572
Stephen Feather <sfeather@gmail.com>
2024-07-30 07:03:09 -0400

    Add ID column to product list

--- loader.php
+++ loader.php
@@ -16,6 +16,7 @@ require_once plugin_dir_path( __FILE__ ) . 'src/Rest/class-importmediaimage.php'
 require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-attachment-sha256-meta-box.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-custom-admin-menu.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-display-vendor.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-display-id.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-category-counts.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-admin-meta-boxes.php';
 
---
+++ src/Admin/class-product-display-id.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Class to add a custom column for ID to the product edit list table.
+ *
+ * @package FA-Toolkit
+ * @since 1.0.8
+ */
+
+namespace FAToolkit\Admin;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class to add a custom column for id to the product list table.
+ */
+class Product_Display_Id {
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_filter( 'manage_edit-product_columns', array( $this, 'add_id_column' ), 20 );
+		add_action( 'manage_product_posts_custom_column', array( $this, 'add_id_column_content' ), 20, 2 );
+	}
+
+	/**
+	 * Adds the id column to the product list table.
+	 *
+	 * @param array $columns The existing columns.
+	 * @return array $columns The updated columns.
+	 */
+	public function add_id_column( $columns ) {
+		global $wpdb;
+		$columns['ID'] = 'ID';
+		ray( $columns );
+		ray( $wpdb->queries );
+		return $columns;
+	}
+
+	/**
+	 * Adds the id column content to the product list table.
+	 *
+	 * @param string $column The column name.
+	 * @param int    $post_id The post ID.
+	 */
+	public function add_id_column_content( $column, $post_id ) {
+		if ( 'ID' === $column ) {
+			echo esc_html( $post_id );
+		}
+	}
+}
+
+new Product_Display_Id();
