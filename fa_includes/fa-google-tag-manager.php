55c1e86f576b2d171c814d301b86692c537b4058
Stephen Feather <sfeather@gmail.com>
2024-07-30 07:21:28 -0400

    Add functions for Google Tag Manager

---
+++ fa_includes/fa-google-tag-manager.php
@@ -0,0 +1,35 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+// =========================================================================
+// GOOGLE TAG MANAGER - <head>
+// =========================================================================
+function add_gtag_to_head() { ?>
+	<!-- Google Tag Manager -->
+	<script defer id='GTM'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NQJ5QVD');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+}
+add_action( 'wp_head', 'add_gtag_to_head' );
+
+// =========================================================================
+// GOOGLE TAG MANAGER - <body>
+// =========================================================================
+function add_gtag_to_body() {
+	?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQJ5QVD"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
+add_action( 'wp_body_open', 'add_gtag_to_body' );
+
+
