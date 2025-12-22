6f3669c71560bf5e9405593e3840f2e52d29c1a6
Stephen Feather <sfeather@gmail.com>
2025-12-22 11:58:21 -0500

    Change the way we import modules

--- fa-toolkit.php
+++ fa-toolkit.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Feather Arms Toolkit
- * Version: 1.0
+ * Version: 1.0.8
  * Description: Collection of WordPress management tools used by Feather Arms.
  * Author: Stephen Feather
  * Author URI: http://stephenfeather.com
@@ -17,7 +17,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 };
 
+add_filter( 'woocommerce_is_purchasable', '__return_true' );
+define( 'FA_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FA_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
+
+require_once FA_TOOLKIT_PATH . 'vendor/autoload.php';
+
 // Load the plugin files.
-require_once plugin_dir_path( __FILE__ ) . 'includes/loader.php';
+require_once FA_TOOLKIT_PATH . 'loader.php';
+require_once FA_TOOLKIT_PATH . 'includes/loader.php';
 
 // Your plugin code goes here.
---
+++ loader.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Loads our src/files.
+ *
+ * @package FA-Toolkit
+ * @since 1.0.8
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+};
+
+
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Rest/class-importmediaimage.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-attachment-sha256-meta-box.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-custom-admin-menu.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-display-vendor.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-display-id.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-product-category-counts.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Admin/class-admin-meta-boxes.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Media/class-media-fix-ilab-metadata.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Media/class-productthumbnailchecker.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Media/class-fetchdraftids.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Promotion/class-promotion.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Promotion/class-promotion-meta-box.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Promotion/class-promotion-posttype.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Product/class-wordcount.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Product/class-bard-meta-box.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Product/class-wc-update-global-unique-id.php';
+// require_once plugin_dir_path( __FILE__ ) . 'src/Product/class-custom-product-status.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Media/class-media-fix-ilab-metadata.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Modules/class-pwbulkeditorsettings.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Modules/class-updraftplussettings.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Modules/class-wpallimportsettings.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Utilities/class-fixrankmathschemas.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Utilities/class-gtins.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Utilities/class-color-test.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/CLI/Tools/class-exportacffield.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/User/class-userregistration.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'src/Site/class-setup.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/Modules/class-querymonitorsettings.php';
+
+//use FAToolkit\Tools\ExportACFField;
+//require __DIR__ . '/vendor/autoload.php';
+
+//new ExportACFField();
