00b4b4b08cd878937404cd7c8d2bf12b7a7a26bc
Stephen Feather <sfeather@gmail.com>
2026-01-04 16:25:42 -0500

    lint: fix spacing

--- src/Media/class-media-fix-ilab-metadata.php
+++ src/Media/class-media-fix-ilab-metadata.php
@@ -93,7 +93,7 @@ class Media_Fix_Ilab_Metadata {
 		$last_processed_post_id = get_option( 'fa_toolkit_last_processed_post_id' );
 		$starting_post_id       = absint( $args[0] ) ?? 0;
 		$override               = $assoc_args['override'] ?? false;
-		$x = $starting_post_id;
+		$x                      = $starting_post_id;
 
 		// Set the order (either 'ASC' for ascending or 'DESC' for descending).
 		$order = 'ASC'; // Use 'DESC' for descending order.
@@ -119,7 +119,7 @@ class Media_Fix_Ilab_Metadata {
 		// Loop through the attachments.
 		foreach ( $attachments as $attachment_id ) {
 			update_option( 'fa_toolkit_last_processed_post_id', $attachment_id );
-							$success = $storage_utilities->fixMetadata( $attachment_id );
+			$success = $storage_utilities->fixMetadata( $attachment_id );
 
 			if ( false === $success ) {
 				\WP_CLI::warning( 'Failed to fix metadata for post ID: ' . $attachment_id );
