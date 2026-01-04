ed9940f0861b1c2f5f10155fcd6659f1c3024e54
Stephen Feather <sfeather@gmail.com>
2026-01-04 15:46:19 -0500

    fix spacing lint error

--- fa-toolkit.php
+++ fa-toolkit.php
@@ -82,7 +82,7 @@ new \FAToolkit\Rest\ImportMediaImage();
 if ( true === defined( 'WP_CLI' ) && true === WP_CLI ) {
 	new \FAToolkit\CLI\Tools\ExportACFField();
 	new \FAToolkit\CLI\Media\ScrapeProductMedia();
-    // TODO: Refactor these classes to use namespacing and autoloading.
+	// TODO: Refactor these classes to use namespacing and autoloading.
 	// The remaining CLI files are procedural and register commands globally.
     // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_require_once
 	require_once __DIR__ . '/src/CLI/Media/class-attachmediatodraftproducts.php';
